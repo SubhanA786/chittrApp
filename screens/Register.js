1c7bc45486022b3d088b6d49309a3c72448ce2dd
SubhanA786 <62364215+SubhanA786@users.noreply.github.com>
2020-03-20 09:16:53 +0000

    Register updated- changed design- validation

--- screens/Register.js
+++ screens/Register.js
@@ -1,84 +1,185 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
 
-import t from 'tcomb-form-native';
+import React, { Fragment } from 'react'
+import { StyleSheet, SafeAreaView, View } from 'react-native'
+import { Button } from 'react-native-elements'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+import FormInput from '../components/FormInput'
+import FormButton from '../components/FormButton'
+import ErrorMessage from '../components/ErrorMessage'
 
-const Form = t.form.Form;
+const validationSchema = Yup.object().shape({
+    given_name: Yup.string()
+        .label('given_name')
+        .required('First name required')
+        .min(2, 'Must have at least 2 characters'),
+    family_name: Yup.string()
+        .label('family_name')
+        .required('Family name required')
+        .min(2, 'Must have at least 2 characters'),
+    email: Yup.string()
+        .label('Email')
+        .email('Enter a valid email')
+        .required('Please enter a registered email'),
+    password: Yup.string()
+        .label('Password')
+        .required()
+        .min(4, 'Password must have more than 4 characters '),
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password')], 'Both passwords do not match')
+        .required('Confirm Password is required')
+})
 
-const userDetails = t.struct({
-    givenName: t.String,
-    familyName: t.String,
-    email: t.String,
-    password: t.String,
-});
+export default class Signup extends React.Component {
+    goToLogin = () => this.props.navigation.navigate('Login')
 
-const formStyles = {
-    ...Form.stylesheet,
-    formGroup: {
-        normal: {
-            marginBottom: 10
-        },
-    },
-    controlLabel: {
-        normal: {
-            color: 'green',
-            fontSize: 18,
-            marginBottom: 7,
-            fontWeight: '600'
-        },
-        // the style applied when a validation error occours
-        error: {
-            color: 'red',
-            fontSize: 18,
-            marginBottom: 7,
-            fontWeight: '600'
+    handleSubmit = values => {
+        if (values.email.length > 0 && values.password.length > 0) {
+            setTimeout(() => {
+                return fetch('http://10.0.2.2:3333/api/v0.0.5/user',{
+                method: 'POST',
+            body: JSON.stringify({
+                given_name: values.given_name,
+                family_name: values.family_name,
+                email: values.email,
+                password: values.password
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then((response) => {
+            alert("User Added!");
+            console.log(JSON.stringify({
+                given_name: values.given_name,
+                family_name: values.family_name,
+                email: values.email,
+                password: values.password
+            }));
+            this.props.navigation.navigate('Login');
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+        }, 3000)
         }
-    }
-}
-
-const options = {
-    fields: {
-        email: {
-            error: 'Without an email address how are you going to reset your password when you forget it?'
-        },
-        password: {
-            error: 'Choose something you use on a dozen other sites or something you won\'t remember'
-        },
-        terms: {
-            label: 'Agree to Terms',
-        },
-    },
-    stylesheet: formStyles,
-};
-
-export default class App extends Component {
-    handleSubmit = () => {
-        const value = this._form.getValue();
-        console.log('value: ', value);
     }
 
     render() {
         return (
-            <View style={styles.container}>
-                <Form
-                    ref={c => this._form = c}
-                    type={userDetails}
-                    options={options}
-                />
+            <SafeAreaView style={styles.container}>
+                <Formik
+                    initialValues={{
+                        given_name: '',
+                        family_name: '',
+                        email: '',
+                        password: '',
+                        confirmPassword: ''
+                    }}
+                    onSubmit={values => {
+                        this.handleSubmit(values)
+                    }}
+                    validationSchema={validationSchema}>
+                    {({
+                          handleChange,
+                          values,
+                          handleSubmit,
+                          errors,
+                          isValid,
+                          touched,
+                          handleBlur,
+                          isSubmitting
+                      }) => (
+                        <Fragment>
+                            <FormInput
+                                name='given_name'
+                                value={values.given_name}
+                                onChangeText={handleChange('given_name')}
+                                placeholder='Enter your first name'
+                                iconName='md-person'
+                                iconColor='#2C384A'
+                                onBlur={handleBlur('given_name')}
+                                autoFocus
+                            />
+                            <ErrorMessage errorValue={touched.given_name && errors.given_name} />
+                            <FormInput
+                                name='family_name'
+                                value={values.family_name}
+                                onChangeText={handleChange('family_name')}
+                                placeholder='Enter your surname/family name'
+                                iconName='md-person'
+                                iconColor='#2C384A'
+                                onBlur={handleBlur('family_name')}
+                                autoFocus
+                            />
+                            <ErrorMessage errorValue={touched.family_name && errors.family_name} />
+                            <FormInput
+                                name='email'
+                                value={values.email}
+                                onChangeText={handleChange('email')}
+                                placeholder='Enter email'
+                                autoCapitalize='none'
+                                iconName='ios-mail'
+                                iconColor='#2C384A'
+                                onBlur={handleBlur('email')}
+                            />
+                            <ErrorMessage errorValue={touched.email && errors.email} />
+                            <FormInput
+                                name='password'
+                                value={values.password}
+                                onChangeText={handleChange('password')}
+                                placeholder='Enter password'
+                                secureTextEntry
+                                iconName='ios-lock'
+                                iconColor='#2C384A'
+                                onBlur={handleBlur('password')}
+                            />
+                            <ErrorMessage errorValue={touched.password && errors.password} />
+                            <FormInput
+                                name='password'
+                                value={values.confirmPassword}
+                                onChangeText={handleChange('confirmPassword')}
+                                placeholder='Confirm password'
+                                secureTextEntry
+                                iconName='ios-lock'
+                                iconColor='#2C384A'
+                                onBlur={handleBlur('confirmPassword')}
+                            />
+                            <ErrorMessage
+                                errorValue={touched.confirmPassword && errors.confirmPassword}
+                            />
+                            <View style={styles.buttonContainer}>
+                                <FormButton
+                                    buttonType='outline'
+                                    onPress={handleSubmit}
+                                    title='SIGNUP'
+                                    buttonColor='#F57C00'
+                                    disabled={!isValid || isSubmitting}
+                                    loading={isSubmitting}
+                                />
+                            </View>
+                        </Fragment>
+                    )}
+                </Formik>
                 <Button
-                    title="Sign Up!"
-                    onPress={this.handleSubmit}
+                    title='Have an account? Login'
+                    onPress={this.goToLogin}
+                    titleStyle={{
+                        color: '#33CCFF'
+                    }}
+                    type='clear'
                 />
-            </View>
-        );
+            </SafeAreaView>
+        )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        justifyContent: 'center',
-        marginTop: 50,
-        padding: 20,
-        backgroundColor: '#ffffff',
+        flex: 1,
+        backgroundColor: '#fff'
     },
-});
+    buttonContainer: {
+        margin: 25
+    }
+})
