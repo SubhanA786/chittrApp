f9573ed2fa43682a478d3570b9c6d606b71509a3
SubhanA786 <62364215+SubhanA786@users.noreply.github.com>
2020-03-20 09:06:56 +0000

    App.js-screens added-login,register,dashboard

--- App.js
+++ App.js
@@ -1,1 +1,49 @@
-//
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './screens/Homepage';
+import SignUp from './screens/Register';
+import LoginScreen from './screens/LoginScreen';
+
+
+
+const login = createStackNavigator({
+        Homepage: {
+            screen: Homepage
+        },
+        Register: {
+            screen: Register
+        },
+        Login: {
+            screen: Login
+        }
+    },
+    {
+        headerMode: "none",
+        initialRouteName: "Homepage"
+    });
+
+
+const loggedIn = createStackNavigator({
+        Dashboard: {
+            screen: Dashboard
+        }
+    },
+    {
+        headerMode: "none",
+        initialRouteName: "Dashboard"
+    });
+
+const AppStackNav = createStackNavigator({
+        Auth: login,
+        App: loggedIn,
+        AuthLoadingScreen: LoadingScreen
+    },
+    {
+        headerMode: "none",
+        initialRouteName: "AuthLoadingScreen"
+    });
+
+
+
+const AppContainer = createAppContainer(AppStackNav)
+export default AppContainer;
